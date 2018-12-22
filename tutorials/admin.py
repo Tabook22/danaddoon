e539ca0af68e437b831050df7f56cee8bdaa3cf3
Nasser Tabook <nassertabook@Nassers-MacBook-Pro.local>
2018-12-22 13:06:34 +0400

    sss

--- tutorials/admin.py
+++ tutorials/admin.py
@@ -1,4 +1,21 @@
 from django.contrib import admin
 from .models import Tutorial
 
-admin.site.register(Tutorial)
+#here am creating a class called TutorialAdmin, this class will be used
+#customize the admin page
+class TutorialAdmin(admin.ModelAdmin):
+    #the name list_display is important to write it exatly as it is otherwise it will not work
+    list_display=('id','title','date_published', 'is_published')
+    #here we need to choose which list item we are going to choose to make them as links
+    list_display_links=('id','title')
+
+    #here we want to filter it by date displyed
+    list_filter=('title',)  # putting the last "comma" is important otherwise will give error
+    ordering = ('-date_published',) # The negative sign indicate descendent order
+    list_editable=('is_published',)   # we need to click on is published to enable that choose in the list
+    search_fields =('title', 'l_desc','date_published') # here we are putting the fields we need to search by
+    
+    #defining number of items or tutorials per page
+    list_per_page=25
+
+admin.site.register(Tutorial,TutorialAdmin)
--- tutorials/views.py
+++ tutorials/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
+from .models import Tutorial 
 
 def tutorials(request):
+    tutorials=Tutorial.objects.all()
+    
     return render(request, 'tutorials/tutorials.html')
 
 def tutorial(request):
